0cd4a6220209a1496980e078b399875e1de0b803
Haley Kang <haleyjkemail@gmail.com>
2026-03-15 20:35:47 -0500

    Added Set and Get functions into header file.

--- functions.h
+++ functions.h
@@ -8,6 +8,39 @@
 #include <vector>
 using namespace std;
 
+struct Pair{
+    string key;
+    string value;
+};
 
+vector<Pair>db;
+
+string filename="data.db";
+
+void setVal(string key, string value){
+    for (int i=0; i<db.size(); i++){
+        if (db[i].key==key){
+            db[i].value=value;
+            goto write;
+        }
+    }
+
+    db.push_back({key, value});
+
+    write:
+        fstream file(filename, ios::app);
+        file<<"SET "<<key<<" "<<value<<endl;
+        file.close();
+}
+
+string getVal(string key){
+    for (int i=0; i<db.size(); i++){
+        if (db[i].key==key){
+            return db[i].value;
+        }
+    }
+
+    return "NULL";
+}
 
 #endif
